5ac63d273a95372d3d5df1267c2b1d44215cdc92
Scott Cox <scottcox89@gmail.com>
2021-01-23 14:25:48 +0000

    Fizzbuzz++

---
+++ node/fizzbuzz++.js
@@ -0,0 +1,50 @@
+const fs = require("fs");
+
+const isFizz = (input) => input % 3 === 0;
+const isBuzz = (input) => input % 5 === 0;
+const isPrime = (input) => {
+  let isPrime = true;
+
+  if (input === 1) {
+    isPrime = false;
+  }
+
+  for (let i = 2; i < input; i++) {
+    if (input % i === 0) {
+      isPrime = false;
+      break;
+    }
+  }
+
+  return isPrime;
+};
+
+const getFizzBuzz = (input) => {
+  let string = `${input}`;
+
+  if (isPrime(input)) {
+    string = string + " FiZZBUZZ++";
+  } else if (isFizz(input) && isBuzz(input)) {
+    string = string + " FIZZBUZZ";
+  } else if (isFizz(input)) {
+    string = string + " FIZZ";
+  } else if (isBuzz(input)) {
+    string = string + " BUZZ";
+  }
+
+  return `${string} \n`;
+};
+
+let fizzBuzzString = "";
+
+for (let i = 1; i <= 500; i += 1) {
+  fizzBuzzString = fizzBuzzString + getFizzBuzz(i);
+}
+
+fs.appendFile("./node/fizzbug.log", fizzBuzzString, (err) => {
+  if (err) {
+    console.error(err);
+  }
+
+  console.log("Writing to fizzbuzz.log");
+});
